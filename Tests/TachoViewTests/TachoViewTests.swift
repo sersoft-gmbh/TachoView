ce1123d1baa29151449abcc022f0dfa7bcc1fdd7
Florian Friedrich <ffried@me.com>
2022-08-01 17:29:34 +0200

    Cleanup

--- Tests/TachoViewTests/TachoViewTests.swift
+++ Tests/TachoViewTests/TachoViewTests.swift
@@ -2,10 +2,9 @@ import XCTest
 @testable import TachoView
 
 final class TachoViewTests: XCTestCase {
-    func testExample() throws {
+    func testExample() async throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        // XCTAssertEqual(TachoView().text, "Hello, World!")
     }
 }
