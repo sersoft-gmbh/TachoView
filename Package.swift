665db412e14cb8fefa61e36d1667a4cd961d54a0
Florian Friedrich <ffried@me.com>
2022-08-01 17:28:23 +0200

    Cleanup Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,11 @@ import PackageDescription
 let package = Package(
     name: "TachoView",
     platforms: [
-        .iOS(.v14),
+        .iOS(.v15),
+        .tvOS(.v15),
+        .watchOS(.v8),
+        .macOS(.v12),
+        .macCatalyst(.v15),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -14,16 +18,10 @@ let package = Package(
             name: "TachoView",
             targets: ["TachoView"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "TachoView",
-            dependencies: []),
+        .target(name: "TachoView"),
         .testTarget(
             name: "TachoViewTests",
             dependencies: ["TachoView"]),
